9cf42edca34f30ea08146bc7aa6a02323aa8a53c
naasirosmanzoro <naasir.osman@zoro.co.uk>
2022-09-28 13:54:41 +0100

    end of 2

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ def hello_world():
 
 print(hello_world())
 # print() runs a function
+
+
+
+
+#  booleans are 0 and 1 in form of false and true
+
+# numbers are either integers (follow BIDMAS) 
+# or floating point decimals
+
+print(17/3) 
+
+# 5.667 is a float point decimal
+# // divides and rounds down
+# % 
+# += *= etc is the same as JavaScript
+# https://www.w3schools.com/python/python_operators.asp
+
+
+
+
+# strings
+
