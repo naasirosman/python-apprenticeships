5f0fb78a174d8fdc1f36905d980b039472a84db1
naasirosmanzoro <naasir.osman@zoro.co.uk>
2022-09-28 14:34:44 +0100

    done 4

--- main.py
+++ main.py
@@ -28,3 +28,96 @@ print(17/3)
 
 # strings
 
+
+
+x = "hello world"
+
+y = """hello 
+world""" 
+
+print(x)
+print(y)
+#  triple "" to do multi-line strings
+
+
+
+
+# combining strings and variables
+test = "bye"
+print("hello " + test) 
+
+# slicing strings
+x = "helloworld"
+print(x[0:5])
+print(x[0:4])
+print(x[1:5])
+print(x[2:5])
+print(x[3:5])
+print(x[5:-1])
+print(x[5:-3])
+
+
+
+# data-types: Lists, Sets, Dictionaries
+
+# lists - usually used for single dimension (same data type) that is redeclarable
+
+a = [99, "string", True, 2.3]
+
+# fetching from a list
+print(a[0])
+
+# slicing a list
+print(a[1:3])
+
+# push into list list
+a.append("hello")
+print(a)
+
+# push into specific position
+
+a.insert(1, "bye")
+print(a)
+
+# find length
+print(len(a))
+
+# replace
+a[1:2] = ["replace1"]
+print(a)
+
+# multidimension list (c contains a and b)
+
+a = [0, 1, 2, 3, 4, 5]
+b = [0, 10, 20, 20, 40, 50]
+c = [a, b]
+print(c)
+
+# you can append (isnert) a list to it self to make infinite list
+c.append(c)
+print(c)
+# python is smart enough to use [...]
+# fetching itself
+print(c[2]) 
+
+
+
+# dictionary are sets inside sets that corrospond (in pairs)
+
+x = {}
+y = { "a":1, "b": 2, "c": 3 }
+print(y["a"])
+
+# adding a new pair to dictionary
+
+y["d"] = 4
+print(y)
+
+# checking inside list
+print("d" in y)
+print("e" in y)
+del(y["d"])
+print("d" in y)
+
+
+
